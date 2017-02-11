0bc82437703ca3520c70bd86f0bcabc173667c13
Nick Nelson <nick34nelson@hotmail.com>
2017-02-10 20:11:48 -0800

    Assembly page COMPLETE.

--- application/controllers/Assembly.php
+++ application/controllers/Assembly.php
@@ -15,10 +15,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author Nick
  */
 class Assembly extends Application {
-    //put your code here
-    public function index()
-	{
+
+    public function index() {
+                $this->data['head1'] = $this->robots->getPart(1)['fileName'];
+                $this->data['body1'] = $this->robots->getPart(2)['fileName'];
+                $this->data['feet1'] = $this->robots->getPart(3)['fileName'];
+                              
+                $this->data['head2'] = $this->robots->getPart(4)['fileName'];
+                $this->data['body2'] = $this->robots->getPart(5)['fileName'];
+                $this->data['feet2'] = $this->robots->getPart(6)['fileName'];
+                              
+                $this->data['head3'] = $this->robots->getPart(7)['fileName'];
+                $this->data['body3'] = $this->robots->getPart(8)['fileName'];
+                $this->data['feet3'] = $this->robots->getPart(9)['fileName'];
+                
+                $this->data['head4'] = $this->robots->getPart(10)['fileName'];
+                $this->data['body4'] = $this->robots->getPart(11)['fileName'];
+                $this->data['feet4'] = $this->robots->getPart(12)['fileName'];
+                              
+                $this->data['head5'] = $this->robots->getPart(13)['fileName'];
+                $this->data['body5'] = $this->robots->getPart(14)['fileName'];
+                $this->data['feet5'] = $this->robots->getPart(15)['fileName'];
+                              
+                $this->data['head6'] = $this->robots->getPart(16)['fileName'];
+                $this->data['body6'] = $this->robots->getPart(17)['fileName'];
+                $this->data['feet6'] = $this->robots->getPart(18)['fileName'];
 		$this->data['pagebody'] = 'assembly';
 		$this->render(); 
-	}
+    } 
 }
