245ffc1969e4a9f4caef49f2c5512695199f1a72
Karan Parmar <karandipparmar@hotmail.com>
2017-03-31 16:24:06 -0700

    Started on the manage page

---
+++ application/views/managepage.php
@@ -0,0 +1,26 @@
+<!-- History page. Displays Contents -->
+
+<div class="container-fluid">
+    <!-- title of the page -->
+    <h2>Manage</h2>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-success">
+            <a href = "egn"><h2>Reboot</h2></a>  
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-success">
+            <a href = "../register"><h2>Register</h2></a>  
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-success">
+            <a href = "egn"><h2>Display/Edit Key Value Pairs</h2></a>  
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-success">
+            <a href = "egn"><h2>Sell bots</h2></a>  
+        </div>
+    </div>
+</div>
