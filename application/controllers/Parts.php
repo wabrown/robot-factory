acd6bf79abb829cdd4588ecd57e993b482fab03a
Nick Nelson <nick34nelson@hotmail.com>
2017-03-31 20:38:15 -0700

    Merge origin/develop into develop
    Conflicts:
    	application/models/Partsdata.php
    	data/factory.sql

--- application/controllers/Parts.php
+++ application/controllers/Parts.php
@@ -13,89 +13,157 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * @author Jake
  */
+class Parts extends Application
+{
 
-class Parts extends Application {
-
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
+        $this->load->model('partsdata');
     }
 
     public function index()
     {
-        $this->data['pagetitle'] = 'Parts List';
-        $this->data['pagebody'] = 'partspage';
+        $user_role = $this->session->userdata('userrole');
+
+        if ($user_role != 'worker')
+        {
+            $this->data['pagetitle'] = 'Allow only worker';
+        }
 
         // Get all parts 
         $allParts = $this->partsdata->get_all_parts();
-        sort($allParts);
-        
-        // Create each cell
+
+        $this->generate_table($allParts);
+    }
+
+    public function generate_table($allParts)
+    {
+        $this->data['pagetitle'] = 'Parts List';
+        $this->data['pagebody'] = 'partspage';
+
+        $head_parts = array();
+        $torso_parts = array();
+        $legs_parts = array();
+
+        // save parts by piece
         foreach ($allParts as $part)
         {
-
-            $cells[] = $this->parser->parse('_cell', (array) $part, true);
-            
+            if ($part['piece'] == '1')
+            {
+                $head_parts[] = array(
+                    'id' => $part['id'],
+                    'model' => $part['model'],
+                    'piece' => $part['piece'],
+                    'plant' => $part['plant'],
+                    'stamp' => $part['stamp'],
+                    'aquired_time' => date("Y-m-d H:i:s", time()),
+                    'file_name' => $part['model'] . $part['piece'] . '.jpeg',
+                    'href' => '/parts/' . $part['id']);
+            } elseif ($part['piece'] == '2')
+            {
+                $torso_parts[] = array(
+                    'id' => $part['id'],
+                    'model' => $part['model'],
+                    'piece' => $part['piece'],
+                    'plant' => $part['plant'],
+                    'stamp' => $part['stamp'],
+                    'aquired_time' => date("Y-m-d H:i:s", time()),
+                    'file_name' => $part['model'] . $part['piece'] . '.jpeg',
+                    'href' => '/parts/' . $part['id']);
+            } elseif ($part['piece'] == '3')
+            {
+                $legs_parts[] = array(
+                    'id' => $part['id'],
+                    'model' => $part['model'],
+                    'piece' => $part['piece'],
+                    'plant' => $part['plant'],
+                    'stamp' => $part['stamp'],
+                    'aquired_time' => date("Y-m-d H:i:s", time()),
+                    'file_name' => $part['model'] . $part['piece'] . '.jpeg',
+                    'href' => '/parts/' . $part['id']);
+            }
         }
-       
-        //$this->data['parts_table'] = $this->parser->parse('partspage', (array) $cells, true);
 
-        
-        $this->load->library('table');
-        $robot_array = array(
-            'table_open' => '<table class="table-bordered">',
-            'heading_cell_start' => '<th class="text-danger header-size">',
-        );
-        $this->table->set_heading('Head Parts', 'Torso Parts', 'Leg Parts');
-        $this->table->set_template($robot_array);
-        $rows = $this->table->make_columns($cells, 3);
-        $this->data['parts_table'] = $this->table->generate($rows);
-
-       
+        $this->data['head_parts'] = $head_parts;
+        $this->data['torso_parts'] = $torso_parts;
+        $this->data['legs_parts'] = $legs_parts;
         $this->render();
     }
-    
+
     public function get_single_page($id)
     {
-
         // load a page for details
         $this->data['pagebody'] = 'singlepage';
 
+        // get single part
         $onePart = $this->partsdata->get_single_part($id);
- 
+
         // merge the records to data array
-        $this->data = array_merge($this->data, (array)$onePart);
+        $this->data = array_merge($this->data, (array) $onePart);
 
         $this->render();
     }
-    
-    /*
-    public function get_random_parts(){
-        
-         $this->data['pagetitle'] = 'TEST get parts certis';
-         $this->data['pagebody'] = 'partspage';
-         
-        $certis = file_get_contents('https://umbrella.jlparry.com/work/mybuilds?key=29151a');
-        echo $certis;
-       $decodedJson = json_decode($certis);
-       
-       
-       foreach ($decodedJson as $part)
-        {
-  
-            $cells[] = $this->parser->parse('_cell2', (array) $part, true);
-        }
-        
-        $this->load->library('table');
-        $robot_array = array(
-            'table_open' => '<table class="table-bordered">',
-            'heading_cell_start' => '<th class="text-danger header-size">',
-        );
-        $this->table->set_heading('Head Parts', 'Torso Parts', 'Leg Parts');
-        $this->table->set_template($robot_array);
-        $rows = $this->table->make_columns($cells, 3);
-        $this->data['parts_table'] = $this->table->generate($rows);
 
-       
-        $this->render();
-    }*/
+
+    public function get_random_parts()
+    {
+        $API_KEY = '3c2262';
+        $json_parts = file_get_contents('https://umbrella.jlparry.com/work/mybuilds?key=' . $API_KEY);
+
+        // decode json
+        $random_parts = json_decode($json_parts, true);
+
+        // create part array
+        $random_parts_to_save = $this->create_part_array($random_parts);
+
+        // insert parts to db
+        $this->partsdata->insert_parts($random_parts_to_save);
+
+        redirect('/parts');
+    }
+
+    public function buy_parts()
+    {
+
+        $API_KEY = '3c2262';
+        $json_parts = file_get_contents('https://umbrella.jlparry.com/work/buybox?key=' . $API_KEY);
+
+        // decode json
+        $buy_parts = json_decode($json_parts, true);
+        
+        // create part array
+        $buy_parts_to_save = $this->create_part_array($buy_parts);
+
+        // insert parts to db
+        $this->partsdata->insert_parts($buy_parts_to_save);
+
+        redirect('/parts');
+    }
+    
+    private function create_part_array($array)
+    {
+        $temp_array = array();
+        
+        foreach ($array as $part)
+        {
+            $temp_array[] = array(
+                'id' => $part['id'],
+                'model' => $part['model'],
+                'piece' => $part['piece'],
+                'plant' => $part['plant'],
+                'stamp' => $part['stamp'],
+                'aquired_time' => date("Y-m-d H:i:s", time()),
+                'file_name' => $part['model'] . $part['piece'] . '.jpeg',
+                'href' => '/parts/' . $part['id']);
+        }
+
+        return $temp_array;
+    }
+    
+    public function get_parts_by_piece(){
+        $temp = $this->partsdata->get_parts_by_piece(1);
+        var_dump($temp);
+    }
+    
 }
--- application/models/Partsdata.php
+++ application/models/Partsdata.php
@@ -11,211 +11,52 @@
  *
  * @author Jake
  */
-
-class Partsdata extends MY_Model
+class Partsdata extends CI_Model
 {
-    
-    /*var $data = array(
-        array('id' => '1',
-            'name' => 'part A-1',
-            'partCode' => 'A1',
-            'CA' => '000001',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'a1.jpeg',
-            'href' => '/parts/1'),
-        array('id' => '2',
-            'name' => 'part A-2',
-            'partCode' => 'A2',
-            'CA' => '000002',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'a2.jpeg',
-            'href' => '/parts/2'),
-        array('id' => '3',
-            'name' => 'part A-3',
-            'partCode' => 'A3',
-            'CA' => '000003',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'a3.jpeg',
-            'href' => '/parts/3'),
-        array('id' => '4',
-            'name' => 'part B-1',
-            'partCode' => 'B1',
-            'CA' => '000004',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'b1.jpeg',
-            'href' => '/parts/4'),
-        array('id' => '5',
-            'name' => 'part B-2',
-            'partCode' => 'B2',
-            'CA' => '000005',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'b2.jpeg',
-            'href' => '/parts/5'),
-        array('id' => '6',
-            'name' => 'part B-3',
-            'partCode' => 'B3',
-            'CA' => '000006',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'b3.jpeg',
-            'href' => '/parts/6'),
-        array('id' => '7',
-            'name' => 'part C-1',
-            'partCode' => 'C1',
-            'CA' => '000007',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'c1.jpeg',
-            'href' => '/parts/7'),
-        array('id' => '8',
-            'name' => 'part C-2',
-            'partCode' => 'C2',
-            'CA' => '000008',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'c2.jpeg',
-            'href' => '/parts/8'),
-        array('id' => '9',
-            'name' => 'part C-3',
-            'partCode' => 'C3',
-            'CA' => '000009',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'c3.jpeg',
-            'href' => '/parts/9'),
-        array('id' => '10',
-            'name' => 'part M-1',
-            'partCode' => 'M1',
-            'CA' => '000010',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'm1.jpeg',
-            'href' => '/parts/10'),
-        array('id' => '11',
-            'name' => 'part M-2',
-            'partCode' => 'M2',
-            'CA' => '000011',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'm2.jpeg',
-            'href' => '/parts/11'),
-        array('id' => '12',
-            'name' => 'part M-3',
-            'partCode' => 'M3',
-            'CA' => '000012',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'm3.jpeg',
-            'href' => '/parts/12'),
-        array('id' => '13',
-            'name' => 'part R-1',
-            'partCode' => 'R1',
-            'CA' => '000013',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'r1.jpeg',
-            'href' => '/parts/13'),
-        array('id' => '14',
-            'name' => 'part R-2',
-            'partCode' => 'R2',
-            'CA' => '000014',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'r2.jpeg',
-            'href' => '/parts/14'),
-        array('id' => '15',
-            'name' => 'part R-3',
-            'partCode' => 'R3',
-            'CA' => '000015',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'r3.jpeg',
-            'href' => '/parts/15'),
-        array('id' => '16',
-            'name' => 'part W-1',
-            'partCode' => 'W1',
-            'CA' => '000016',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'w1.jpeg',
-            'href' => '/parts/16'),
-        array('id' => '17',
-            'name' => 'part W-2',
-            'partCode' => 'W2',
-            'CA' => '000017',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'w2.jpeg',
-            'href' => '/parts/17'),
-        array('id' => '18',
-            'name' => 'part W-3',
-            'partCode' => 'W3',
-            'CA' => '000018',
-            'builtAt' => 'Strawberry',
-            'builtTime' => 'Jan-01-2017',
-            'aquiredTime' => 'Jan-30-2017',
-            'part' => 'head',
-            'fileName' => 'w3.jpeg',
-            'href' => '/parts/18'),
-    );*/
-     
-     
-    
-    
+
     // Constructor    
     public function __construct()
     {
         parent::__construct('partsdata', 'id');
     }
 
-    // retrieve all of the parts
+    // retrieve all of the parts from db
     public function get_all_parts()
     {
-        return $this->all();
-
+        return $this->db->get('partsdata')->result_array();
     }
 
     // retrieve a single part
-    public function getPart($which) {
+    public function get_single_part($which)
+    {
+        $all_parts = $this->get_all_parts();
+
+        // iterate over the data until we find the one we want
+        foreach ($all_parts as $one_part)
+        {
+            if ($one_part['id'] == $which)
+            {
+                return $one_part;
+            }
+        }
+        return null;
+    }
+
+    // insert parts to db
+    public function insert_parts($parts)
+    {
+        $this->db->insert_batch('partsdata', $parts);
+    }
+
+    public function get_parts_by_piece($piece)
+    {
+        $this->db->select('*')->from('partsdata');
+        $this->db->where('piece', $piece);
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+    
+        public function getPart($which) {
         $allParts = $this->all();
         foreach ($allParts as $onePart) {
             $id = $onePart->id;
@@ -223,22 +64,20 @@ class Partsdata extends MY_Model
                 return $onePart;
             }
         }
-    }    
-    public function get_single_part($which)
+    }
+    
+     public function deletePartById($id)
     {
-        $all_parts = $this->all();
-        
-        // iterate over the data until we find the one we want
-        foreach ($all_parts as $one_part)
-        {
-            $id = $one_part->id;
-            
-            if($id == $which)
-            {
-                return $one_part;
-            }
-        }
-        return null;
+        $this->db->where('id', $id);
+        $this->db->delete('partsdata');
+    }
+
+    public function getPartByFile($filename)
+    {
+        $this->db->select('*')->from('partsdata');
+        $this->db->where('file_name', $filename);
+        $query = $this->db->get();
+        return $query->result_array();
     }
 
 }
