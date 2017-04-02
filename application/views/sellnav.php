6ca6e0ec2873b582bc4d76d514df831d88b1ead2
wabrown <crunchymail27@gmail.com>
2017-04-01 21:03:07 -0700

    added ACTUAL sellbots
    Signed-off-by: wabrown <crunchymail27@gmail.com>

---
+++ application/views/sellnav.php
@@ -0,0 +1,7 @@
+<ul class="nav nav-pills">
+        <li><b>Pages</b></li>
+        <li><a href="/Sell/page/{first}">First</a></li>
+        <li><a href="/Sell/page/{previous}">Previous</a></li>
+        <li><a href="/Sell/page/{next}">Next</a></li>
+        <li><a href="/Sell/page/{last}">Last</a></li>
+</ul>
